dac0bc76ca7a0a144d54fc46f93ca4722829aa9f
rogersaloo <owitrogers@gmail.com>
2023-06-23 19:41:04 +0400

    add roberta model full

--- roberta/README.md
+++ roberta/README.md
@@ -7,3 +7,8 @@ The repository contains the roberta model from hugging face in order to perform 
 2. Create a new virtual environment ```python -m venv venv```
 3. for windows user ```.\venv\Scripts\activate ``` to activate the environment.
 for mac users ```python -m venv venv```
+
+
+## Resources
+1. Inspiration from the roberta sentimental model [model](https://www.youtube.com/watch?v=uPKnSq6TaAk)
+2. Hugging face roberta model ```https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment```
--- roberta/main.py
+++ roberta/main.py
@@ -1,18 +1,57 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, \
+    AutoModelForSequenceClassification
 from scipy.special import softmax
 
 chat = "@rogers aloo today we are cebrating the entire period"
 
-chat_words = []
 
-for word in chat.split(" "):
-    if word.startswith("@") and len(word) >1:
-        word = "@user"
-        
-    elif word.startswith("http"):
-        word = "http"
-    chat_words.append(word)
+def split_chat(agent_chat:str):
+    """Split the chats recived from the customer by the agent
+
+    Args:
+        agent_chat (str): chat from the customer
+
+    Returns:
+        _type_: split and joined words from the chat
+    """
+    chat_words = []
+
+    for word in agent_chat.split(" "):
+        if word.startswith("@") and len(word) >1:
+            word = "@user"
+            
+        elif word.startswith("http"):
+            word = "http"
+        else:
+            chat_words.append(word)
+
+    return " ".join(chat_words)
 
 
+# load the model to use for the sentiment anlysis
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+labels = ["Negative", "Neutral", "Positive"]
 
-print(chat_words)
+
+def sentiment_anlysis(joined_chat:str):
+    """Performs a sentimental anlysis on the chats
+
+    Args:
+        joined_chat (str): string of the split and joined chat from customer
+
+    Returns:
+        _type_: split and joined words from the cha
+    """
+    encoded_chat = tokenizer(joined_chat, return_tensors= 'pt')
+    output = model(encoded_chat[''])
+
+def main():
+    joined = split_chat(chat)
+    sentiment_anlysis(joined)
+
+
+if __name__ == '__main__':
+    main()
+
